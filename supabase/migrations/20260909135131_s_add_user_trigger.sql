673d4eed71ee6fa01e1d047f7a5327a5563556b4
Ssword-Dev <ssword.dev@gmail.com>
2026-09-11 19:43:44 +0800

    fix add user trigger migration

--- supabase/migrations/20260909135131_s_add_user_trigger.sql
+++ supabase/migrations/20260909135131_s_add_user_trigger.sql
@@ -15,6 +15,6 @@ end;
 $$ language plpgsql security definer;
 
 create or replace trigger on_auth_user_created
-  after insert on auth.user_data
+  after insert on auth.users
   for each row
   execute function app.handle_new_user();
