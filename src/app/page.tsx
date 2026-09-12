deabb752e09326399ba320cfafbd35c131a31ce8
Ssword-Dev <ssword.dev@gmail.com>
2026-09-12 14:12:22 +0800

    fix index page stubbing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Page() {
         this is only a stub so next typegen can generate route "/".
         this page is a virtual page that resolves via the proxy.
     */
-    throw new Error("Not implemented.");
+    return null;
 }
